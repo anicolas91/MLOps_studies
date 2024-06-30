b54d80b5c37194305c2ddfc992ab84bc22da25b2
anicolas91 <andreanicolas91@gmail.com>
2024-06-30 04:23:21 -0700

    feat(run.sh): added integration test bash runfile
    actually has not run, in theory should work, needs to be tried out
    
    n

--- 06-best-practices/homework/tests/run.sh
+++ 06-best-practices/homework/tests/run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 cd  "$(dirname "$0")"
 
 LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
@@ -8,8 +9,25 @@ docker build -t ${LOCAL_IMAGE_NAME} ..
 
 export PREDICTIONS_STREAM_NAME="nycmarch2023predictions"
 
-docker-compose up
+docker-compose up s3 -d # releases the terminal if you use -d
+
+sleep 5
+
+export S3_ENDPOINT_URL="http://localhost:4566" # just in case
 
 export INPUT_FILE_PATTERN="s3://nyc-duration/in/{year:04d}-{month:02d}.parquet"
 export OUTPUT_FILE_PATTERN="s3://nyc-duration/out/{year:04d}-{month:02d}.parquet"
 
+aws --endpoint-url=http://localhost:4566 s3 mb s3://nyc-duration
+
+pipenv run python integration_test.py
+
+ERROR_CODE=$?
+
+if [ ${ERROR_CODE} != 0 ]; then
+    docker-compose logs
+    docker-compose down
+    exit ${ERROR_CODE}
+fi
+
+docker-compose down
